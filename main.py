39e11b29bf62c54c8c690d48ce6a4e6aa3a60da2
Даниил <144156334+JoKeRqZzZ@users.noreply.github.com>
2023-10-30 16:26:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,52 @@
 import math
 try:
-    number1 = int(input("Введите number1: "))
-    number2 = int(input("Введите number2: "))
-
-    print("действия")
+    num1 = int(input("Введите number1 : "))
+    num2 = int(input("Введите number2 : "))
+    print("действия :")
     print("1 - сложение двух чисел")
     print("2 - вычитание второго числа из первого")
-    print("3 - умножение двух чисел")
-    print("4 - деление первого числа на второе")
-    print("5 - возведение первого числа в степень второго")
+    print("3 - умножение двух чисен")
+    print("4 - деление первого числа на второе ")
+    print("5 - возведение певрого числа на второе")
     print("6 - квадратный корень первого и второго числа")
     print("7 - факториал первого и второго числа")
     print("8 - синус первого числа")
-    print("9 - косинус первого числа")
+    print("9 - косинус первого  числа")
     print("10 - тангенс первого числа")
-
-    deystviye = input("Введите номер действия: ")
-
-
-    deystviye = input("Введите номер действия: ")
-
+    deystviye = input("Введите номер действия ")
     if deystviye == "1":
-        print(number1 + number2)
+        print(num1+num2)
     elif deystviye == "2":
-        print(number1 - number2)
+        print(num1-num2)
     elif deystviye == "3":
-        print(number1 * number2)
+        print(num1*num2)
     elif deystviye == "4":
-        if number2 == 0:
-            print("деление на 0 нельзя")
+        if num2 ==0:
+            print("деление на 0 нельзя!!!")
         else:
-            print(number1 / number2)
+             print(num1/num2)
     elif deystviye == "5":
-        print(number1 ** number2)
+        print(num1**num2)
     elif deystviye == "6":
-        print(math.sqrt(number1), math.sqrt(number2))
+        print(math.sqrt(num1), math.sqrt(num2))
     elif deystviye == "7":
-        print(math.factorial(number1), math.factorial(number2))
-    elif deystviye == "8":
-        number1 = int(input("Напишите количество радианов: "))
-        radianov = math.radians(number1)
+        if num1 <= 0 or num2 <= 0:
+            print("чел ты")
+        else:
+            print(math.factorial(num1), math.factorial(num2))
+    elif deystviye == "8" :
+        num1 = int((input("Напишите количество радианов")))
+        radianov=math.radians(num1)
         print(math.sin(radianov))
     elif deystviye == "9":
-        number1 = int(input("Напишите количество радианов: "))
-        radianov = math.radians(number1)
+        num1 = int((input("Напишите количество радианов")))
+        radianov=math.radians(num1)
         print(math.cos(radianov))
     elif deystviye == "10":
-        number1 = int(input("Напишите количество радианов: "))
-        radianov = math.radians(number1)
+        num1 = int((input("Напишите количество радианов")))
+        radianov=math.radians(num1)
         print(math.tan(radianov))
     else:
-        print("ошибка")
+        print("ошибка!!!")
 except ValueError:
-    print("Ошибка ввода числа")
+    print("bad")
